892e441cd7e69e3d09e2ad6aa483b68e214aeb23
palash sarkar <spalash521@gmail.com>
2023-11-11 23:43:11 +0300

    add all my code

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,12 +1,68 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
 
 const videoSchema = new Schema(
     {
+        videoFile: {
+            type: String,
+            required: true,
+
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+            
+        },
+      title : {
+            type: String,
+            required: true,
+            
+        },
+        description: {
+            type: String,
+            required: true,
+            
+        },
+      duration: {
+            type: Number,
+            required: true,
+            
+        },
+        views: {
+            type: Number,
+            default: 0,
+            
+        },
+        isPublished: {
+            type: Boolean,
+            default:true,
+        },
+
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+      
 
     },
     {
         timestamps: true,
     }
 )
+userSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) return next();
+        
+    
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+userSchema.methods.isPasswordCorrect = async function
+    (password) {
+    return await bcrypt.compare(password, this.password)
+    }
 
 export const Video = mongoose.model("Video", videoSchema)
