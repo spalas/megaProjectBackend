b6d737e82c591d60d2692a1c11ce66a6d9dd6bd7
palash sarkar <spalash521@gmail.com>
2023-11-11 16:47:05 +0300

    add schema

---
+++ src/models/video.model.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+    {
+
+    },
+    {
+        timestamps: true,
+    }
+)
+
+export const Video = mongoose.model("Video", videoSchema)
