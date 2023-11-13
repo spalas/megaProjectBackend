a2c94df2b8d2902419a8656c646a673e6a369877
palash sarkar <spalash521@gmail.com>
2023-11-13 14:20:58 +0300

    insert my multer files

--- src/utils/cloudiary.js
+++ src/utils/cloudiary.js
@@ -13,18 +13,22 @@ const uploadOneCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
         // upload the file on cloudinary
-        cloudinary.uploader.upload(localFilePath, {
+        const response = cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        // file has been successfully uploaded
+      // file has been successfully uploaded
+      console.log("file is uploaded successfully on cloudinary", (await response).url)
+      return response
         
     } catch (error) {
-        
+      fs.unlinkSync(localFilePath)   //remove the locally saved temporary file as the upload operation got rejected
+      return null;
     }
 }
 
+export { uploadOneCloudinary}
 
 
-cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
